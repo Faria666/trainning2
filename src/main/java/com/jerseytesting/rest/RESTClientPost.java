2d1e6fc97e0b32034db91883f4a41ee2176c2c7b
joao-faria <jpfaria@criticalsoftware.com>
2018-09-12 13:14:09 +0100

    6th commit 12/9 - change JDBC function

--- src/main/java/com/jerseytesting/rest/RESTClientPost.java
+++ src/main/java/com/jerseytesting/rest/RESTClientPost.java
@@ -171,10 +171,7 @@ public class RESTClientPost {
         if(request != null)
             answer = client(request);
         if(answer.getOperation().compareTo("none")!=0 ) {
-            int aux = JDBCConnection(answer, request);
-            if(aux == -1){
-                JDBCConnection(answer, request);
-            }
+            JDBCConnection(answer, request);
         }
     }
 
@@ -241,7 +238,7 @@ public class RESTClientPost {
      * @param request is the request made to the server
      */
 
-    private static int JDBCConnection(final Answer answer, final Request request) {
+    private static void JDBCConnection(final Answer answer, final Request request) {
 
         final String jdbc = "jdbc:postgresql://localhost:5432/postgres";//...localhost:5432/jpfar...
         final String username = "postgres";//"jpfar";
@@ -263,6 +260,7 @@ public class RESTClientPost {
             ResultSet rs = stmt.executeQuery("SELECT max(idr) from requests");
             if(rs.next()) {
                 id = rs.getInt(1);
+                //System.out.println(id);
             }
 
             id++;
@@ -288,7 +286,6 @@ public class RESTClientPost {
 
         } catch (SQLException e) {
             e.printStackTrace();
-            return -1;
         } finally {
             try {
 
@@ -302,7 +299,6 @@ public class RESTClientPost {
                 e.printStackTrace();
             }
         }
-        return id;
     }
 
 
