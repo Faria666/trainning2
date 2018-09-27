948a1fc5c0a1e35cfb0529f28886d216d7727c74
joao-faria <jpfaria@criticalsoftware.com>
2018-09-27 14:26:57 +0100

    test file aux

--- src/test/java/com/jerseytesting2/unitTesting.java
+++ src/test/java/com/jerseytesting2/unitTesting.java
@@ -1,5 +1,7 @@
-package com.jerseytesting2.aux;
+package com.jerseytesting2;
 
+import com.jerseytesting2.aux.Answer;
+import com.jerseytesting2.aux.Request;
 import com.jerseytesting2.rest.RESTServer;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,7 +9,7 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 
-public class auxTest {
+public class unitTesting {
 
     @Test
     public void testRequest(){
@@ -20,7 +22,7 @@ public class auxTest {
 
     @Test
     public void testRequestAccept() throws IOException {
-        String request = "{"+ "\"" + "a"+"\""+":"+"\""+"12"+"\""+", "+"\""+"b"+"\""+":"+"\""+"67"+"\""+", "+"\""+"op"+"\""+":"+"\""+"add"+"\""+"}";
+        String request = "{"+ "\"" + "value1"+"\""+":"+"\""+"12"+"\""+", "+"\""+"value2"+"\""+":"+"\""+"67"+"\""+", "+"\""+"operation"+"\""+":"+"\""+"add"+"\""+"}";
         RESTServer server = new RESTServer();
         Response response = server.postRequest(request);
         Assert.assertEquals(Response.Status.OK.getStatusCode(),response.getStatus());
@@ -29,7 +31,7 @@ public class auxTest {
 
     @Test
     public void testRequestAccept2() throws IOException {
-        String request = "{"+ "\"" + "a"+"\""+":"+"\""+"12"+"\""+", "+"\""+"b"+"\""+":"+"\""+"67"+"\""+", "+"\""+"op"+"\""+":"+"\""+"mult"+"\""+"}";
+        String request = "{"+ "\"" + "value1"+"\""+":"+"\""+"12"+"\""+", "+"\""+"value2"+"\""+":"+"\""+"67"+"\""+", "+"\""+"operation"+"\""+":"+"\""+"mult"+"\""+"}";
         RESTServer server = new RESTServer();
         Response response = server.postRequest(request);
         Assert.assertEquals(Response.Status.OK.getStatusCode(),response.getStatus());
@@ -38,7 +40,7 @@ public class auxTest {
 
     @Test
     public void testRequestAccept3() throws IOException {
-        String request = "{"+ "\"" + "a"+"\""+":"+"\""+"12"+"\""+", "+"\""+"b"+"\""+":"+"\""+"67"+"\""+", "+"\""+"op"+"\""+":"+"\""+"div"+"\""+"}";
+        String request = "{"+ "\"" + "value1"+"\""+":"+"\""+"12"+"\""+", "+"\""+"value2"+"\""+":"+"\""+"67"+"\""+", "+"\""+"operation"+"\""+":"+"\""+"div"+"\""+"}";
         RESTServer server = new RESTServer();
         Response response = server.postRequest(request);
         Assert.assertEquals(Response.Status.OK.getStatusCode(),response.getStatus());
@@ -47,7 +49,7 @@ public class auxTest {
 
     @Test
     public void testRequestAccept4() throws IOException {
-        String request = "{"+ "\"" + "a"+"\""+":"+"\""+"12"+"\""+", "+"\""+"b"+"\""+":"+"\""+"67"+"\""+", "+"\""+"op"+"\""+":"+"\""+"avg"+"\""+"}";
+        String request = "{"+ "\"" + "value1"+"\""+":"+"\""+"12"+"\""+", "+"\""+"value2"+"\""+":"+"\""+"67"+"\""+", "+"\""+"operation"+"\""+":"+"\""+"avg"+"\""+"}";
         RESTServer server = new RESTServer();
         Response response = server.postRequest(request);
         Assert.assertEquals(Response.Status.OK.getStatusCode(),response.getStatus());
@@ -56,52 +58,11 @@ public class auxTest {
 
     @Test
     public void testRequestAccept5() throws IOException {
-        String request = "{"+ "\"" + "a"+"\""+":"+"\""+"12"+"\""+", "+"\""+"b"+"\""+":"+"\""+"67"+"\""+", "+"\""+"op"+"\""+":"+"\""+"adkgukgd"+"\""+"}";
+        String request = "{"+ "\"" + "value1"+"\""+":"+"\""+"12"+"\""+", "+"\""+"value2"+"\""+":"+"\""+"67"+"\""+", "+"\""+"operation"+"\""+":"+"\""+"adkgukgd"+"\""+"}";
         RESTServer server = new RESTServer();
         Response response = server.postRequest(request);
         Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
 
-    }
-
-    @Test
-    public void testRequestFail2() throws IOException {
-        String request = "olyjh";
-        RESTServer server = new RESTServer();
-        Response response = server.postRequest(request);
-        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
-
-    }
-
-    @Test
-    public void testRequestFail3() throws IOException {
-        String request = "";
-        RESTServer server = new RESTServer();
-        Response response = server.postRequest(request);
-        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
-    }
-
-    @Test
-    public void testRequestFail4() throws IOException {
-        String request = null;
-        RESTServer server = new RESTServer();
-        Response response = server.postRequest(request);
-        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
-    }
-
-    @Test
-    public void testRequestFail5() throws IOException {
-        String request = "12345";
-        RESTServer server = new RESTServer();
-        Response response = server.postRequest(request);
-        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
-    }
-
-    @Test
-    public void testRequestFail6() throws IOException {
-        String request = " ";
-        RESTServer server = new RESTServer();
-        Response response = server.postRequest(request);
-        Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus());
     }
 
     @Test
